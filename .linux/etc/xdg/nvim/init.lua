da4f9e7279d0b703ef939a2cfcd7c2028f0dbc98
Lattice Sum <dsoyet@outlook.com>
2026-07-28 23:14:36 +0800

    Update etc/xdg/nvim/init.lua

--- .linux/etc/xdg/nvim/init.lua
+++ .linux/etc/xdg/nvim/init.lua
@@ -1,2 +1,24 @@
-vim.opt.shada = "'100,<50,s10,h"
+-- 1. 禁用鼠标
 vim.opt.mouse = ""
+
+-- 2. 记住上次打开位置
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lnum = mark[1]
+        local col = mark[2]
+        if lnum > 1 and lnum <= vim.fn.line("$") then
+            vim.api.nvim_win_set_cursor(0, { lnum, col })
+        end
+    end,
+})
+
+-- 可选：显示行号
+vim.opt.number = true
+
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+
+-- 可选：启用相对行号
+-- vim.opt.relativenumber = true
+
